a918cc0f23e7e25e28fca6392b9f9b24fa2cc31a
The-Liam-Blair <blair.liam@hotmail.co.uk>
2021-02-03 11:27:02 +0000

    Added MongoDB functionality.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,32 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Connect to MongoDb locally on port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+
+        // Init database
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+
+        // Retrieve MongoDb collection
+        MongoCollection<Document> collection = database.getCollection("test");
+
+        // Create a stored document
+        Document doc = new Document("name", "Liam Blair")
+            .append("class", "Software Engineering Methods")
+            .append("year", "2012")
+            .append("result", new Document("CW", 95).append("EX", 85));
+
+        // Document added to the collection
+        collection.insertOne(doc);
+
+        // Check for document in the collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
